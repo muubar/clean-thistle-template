dcea1352d910e9892c1bea003d93f1e58aa16311
Ahmed <destiny_hacker@hotmail.com>
2017-10-08 02:38:35 +0200

    project initialization
    modules and build process initialization

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const purify = require('gulp-purify-css');
+const cleanCSS = require('gulp-clean-css');
+var webpack = require('webpack-stream');
+
+
+function html() {
+  return gulp.src('src/*.html')
+    .pipe(gulp.dest('dist/'));
+};
+
+function sassy() {
+  return gulp.src('src/scss/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(purify(['dist/bundle.js', 'src/*.html']))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist/'));
+};
+
+function webpk() {
+  return gulp.src('src/js/main.js')
+    .pipe(webpack(require('./webpack.config.js')))
+    .pipe(gulp.dest('dist/'));
+};
+
+gulp.task('build', gulp.series(html, webpk, sassy));
+
+gulp.watch(['src/*.html', 'src/scss/style.scss', 'src/js/main.js'], gulp.series(html, webpk, sassy));
