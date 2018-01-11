6b77f6fddb1a9d062c7869bc1479b04c631ff7ea
Ahmed <destiny_hacker@hotmail.com>
2018-01-11 19:20:57 +0200

    implement portfolio projects toggle buttons

--- src/js/main.js
+++ src/js/main.js
@@ -7,9 +7,33 @@ $(document).ready(function () {
 
   //toggle btn-project-active class between portfolio buttons
   $(".btn-project").click(function () {
-    if ($(this)[0] !== $(".btn-project-active")[0]) {
-      $(".btn-project").not($(this)).removeClass("btn-project-active");
+    if (this !== $(".btn-project-active")[0]) {
+      $(".btn-project-active").removeClass("btn-project-active");
+      //$(".btn-project").not($(this)).removeClass("btn-project-active");
       $(this).toggleClass("btn-project-active");
+    }
+
+    switch (this.id) {
+      case "btn-project-1":
+        toggleCarousel($("#btn-project-1")[0], $(".carousel-1")[0]);
+        break;
+      case "btn-project-2":
+        toggleCarousel($("#btn-project-2")[0], $(".carousel-2")[0]);
+        break;
+      case "btn-project-3":
+        toggleCarousel($("#btn-project-3")[0], $(".carousel-3")[0]);
+        break;
+      case "btn-project-4":
+        toggleCarousel($("#btn-project-4")[0], $(".carousel-4")[0]);
+        break;
     }
   })
 });
+
+function toggleCarousel(btn, car) {
+  console.log("yes")
+  if (car !== $(".carousel-active")[0]) {
+    $(".carousel-active").removeClass("carousel-active");
+    $(car).toggleClass("carousel-active");
+  }
+}
